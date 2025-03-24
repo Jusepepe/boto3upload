53036bff32898a05bf3e90a8670df7b798277f07
Jusepepe <saltamontehz@gmail.com>
2025-03-24 18:48:27 -0500

    Subiendo imagen al bucket

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import boto3
+import logging
+from botocore.exceptions import ClientError
+import os
 
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+
+
+def upload_file(file_name, bucket, object_name=None):
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+print(upload_file('image1.jpg','citric-bucket','images/image1.jpg'))
