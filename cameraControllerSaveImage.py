4cf9918ce6427a584774ac7ae92071f9766728a1
Jusepepe <saltamontehz@gmail.com>
2025-04-20 21:39:18 -0500

    Arreglando script guardado

--- cameraControllerSaveImage.py
+++ cameraControllerSaveImage.py
@@ -4,6 +4,6 @@ from time import sleep
 
 picam2 = Picamera2()
 capture_config = picam2.create_still_configuration()
-picam2.start(show_preview=True)
+picam2.start(show_preview=False)
 sleep(1)
 picam2.switch_mode_and_capture_file(capture_config, "image.jpg")
