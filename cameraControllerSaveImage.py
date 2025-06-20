7aee48f99236c17bdbc938fb36b3800a81ca3d53
Jusepepe <saltamontehz@gmail.com>
2025-06-20 17:28:09 -0500

    Corrección código

--- cameraControllerSaveImage.py
+++ cameraControllerSaveImage.py
@@ -5,6 +5,7 @@ from picamera2 import Picamera2, Preview
 picam1 = Picamera2(0)
 picam2 = Picamera2(1)
 picam1.start_preview(Preview.QT)
+picam2.start_preview(Preview.QT)
 
 preview_config1 = picam1.create_preview_configuration()
 preview_config2 = picam2.create_preview_configuration()
@@ -14,9 +15,11 @@ picam2.configure(preview_config2)
 picam1.start()
 picam2.start()
 
-while True:
-    except KeyBoardInterrupt:
-        picam1.stop()
-        picam2.stop()
-        break;
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    picam1.stop()
+    picam2.stop()
+    
 
