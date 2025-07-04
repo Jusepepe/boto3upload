85646de886cacd453a2230b570903117fd908af9
Jusepepe <saltamontehz@gmail.com>
2025-07-04 16:58:59 -0500

    actualizando test

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from PanTilt import PanTilt
+import sys
 
 pan_tilt_1: PanTilt = PanTilt(17, 27)
 pan_tilt_2: PanTilt = PanTilt(23, 24)
 
-pan_tilt_1.reset()
-pan_tilt_2.reset()
+args = sys.argv
+
+print(args)
+pan_tilt_1.pan.set_angle(args[1])
+pan_tilt_2.pan.set_angle(args[2])
+pan_tilt_1.tilt.set_angle(args[3])
+pan_tilt_2.tilt.set_angle(args[4])
 
