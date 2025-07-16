cc4ccd311d9dd5d3d4b86654e78d7b583eb79544
Jusepepe <saltamontehz@gmail.com>
2025-07-16 13:29:20 -0500

    Probando datos que lleguan

--- move_rail.py
+++ move_rail.py
@@ -1,10 +1,4 @@
 from controlESP import moveForward, moveBack, checkLimitSwitch, returnToInitialPosition
 import time
 
-while not checkLimitSwitch()[0]:
-    print("Moviendo hacia adelante")
-    moveForward()
-    time.sleep(1)
-
-print("Moviendo al inicio")
-returnToInitialPosition()
+print(checkLimitSwitch())
