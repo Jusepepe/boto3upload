bc283c37200bfc94733aa22229fcd2face391875
Jusepepe <saltamontehz@gmail.com>
2025-07-16 13:23:50 -0500

    Prints en script para verificar

--- move_rail.py
+++ move_rail.py
@@ -2,7 +2,9 @@ from controlESP import moveForward, moveBack, checkLimitSwitch, returnToInitialP
 import time
 
 while not checkLimitSwitch()[1]:
+    print("Moviendo hacia adelante")
     moveForward()
     time.sleep(1)
 
+print("Moviendo al inicio")
 returnToInitialPosition()
