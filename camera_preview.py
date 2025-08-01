0c7d17dfd03b570832dcded1929706f630cb5cca
Jusepepe <saltamontehz@gmail.com>
2025-08-01 14:48:40 -0500

    GLP Preview

--- camera_preview.py
+++ camera_preview.py
@@ -1,12 +1,12 @@
 from picamera2 import Picamera2
-from picamera2.previews.drpreview import DRMPreview
+from picamera2.previews.gl_preview import GLPreview
 import time
 
 picam2 = Picamera2()
-preview = DRMPreview()
+preview = GLPreview()
 picam2.start_preview(preview)
 picam2.start()
 
-time.sleep(10)  # Preview durante 10 segundos
+time.sleep(10)
 
 picam2.stop()
