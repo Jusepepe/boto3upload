46ee05433b8610a65007bec80cda49147d9ef1e2
Jusepepe <saltamontehz@gmail.com>
2025-08-25 20:28:38 -0500

    Fixing input type to yolo model

--- cv_models/base.py
+++ cv_models/base.py
@@ -1,4 +1,6 @@
 class ObjectDetectionModel:
+    def __init__(self):
+        pass
     def process(self, image_data: bytes):
         """Run inference on the image (bytes). Must be implemented by subclasses."""
         raise NotImplementedError
--- cv_models/yolo_11s.py
+++ cv_models/yolo_11s.py
@@ -1,9 +1,15 @@
 import time
 from .base import ObjectDetectionModel
 from ultralytics import YOLO
+from PIL import Image
+import io
 
 class Yolo11s(ObjectDetectionModel):
+    def __init__(self):
+        super().__init__()
+        self.model = YOLO("cv_models/weights/my_yolo11s.pt")
+    
     def process(self, image_data: bytes):
-        model = YOLO("cv_models/weights/my_yolo11s.pt")
-        results = model.predict(image_data)
+        image_data = Image.open(image_data).convert("RGB")
+        results = self.model.predict(image_data)
         return results
