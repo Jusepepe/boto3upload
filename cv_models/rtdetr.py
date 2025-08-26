dd431aa501ddd67e4d42e7f46701d3c86a9fa3a7
Jusepepe <saltamontehz@gmail.com>
2025-08-26 10:15:20 -0500

    Cambiando importación de RTDETR como Ultralytics RTDETR

--- cv_models/rtdetr.py
+++ cv_models/rtdetr.py
@@ -1,11 +1,11 @@
 from .base import ObjectDetectionModel
-from ultralytics import RTDETR
+from ultralytics import RTDETR as UltralyticsRTDETR
 from PIL import Image
 
 class RTDETR(ObjectDetectionModel):
     def __init__(self):
         super().__init__()
-        self.model = RTDETR("cv_models/weights/rtdetr.pt")
+        self.model = UltralyticsRTDETR("cv_models/weights/rtdetr.pt")
     
     def process(self, image_data: bytes):
         image_data = Image.open(image_data).convert("RGB")
