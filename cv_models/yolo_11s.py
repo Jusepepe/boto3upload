e2d5c888660351c8fb33c413733ff57b84e14440
Jusepepe <saltamontehz@gmail.com>
2025-08-25 20:22:28 -0500

    Yolo11s implementation fixing weights call

--- cv_models/yolo_11s.py
+++ cv_models/yolo_11s.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 
 class Yolo11s(ObjectDetectionModel):
     def process(self, image_data: bytes):
-        model = YOLO("my_yolo11s.pt")
+        model = YOLO("weights/my_yolo11s.pt")
         results = model.predict(image_data)
         return results
